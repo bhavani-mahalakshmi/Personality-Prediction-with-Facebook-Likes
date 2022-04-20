08b38e52ee7bbb8e430c1be386be2fe8d6f0c36a
Madhura <mganga@asu.edu>
2022-04-19 19:24:11 -0700

    SVD code is integrated

--- dataProcessing.R
+++ dataProcessing.R
@@ -1,5 +1,9 @@
 #install needed packages
 install.packages("Matrix")
+install.packages("irlba")
+install.packages("corrplot")
+install.packages("ggplot2", "reshape2")
+install.packages("reshape2")
 
 #matrix library required for construction of sparse matrix
 require(Matrix)
@@ -29,3 +33,46 @@ ulMatrix <- sparseMatrix(i = ulTable$user_index, j = ulTable$like_index, x = 1)
 #change row and col names from row index to userid and text
 rownames(ulMatrix) <- usersTable$userid
 colnames(ulMatrix) <- likesTable$text
+
+# Remove the values that are under threshold
+repeat {                                       
+  i <- sum(dim(ulMatrix))                             
+  ulMatrix <- ulMatrix[rowSums(ulMatrix) >= 50, colSums(ulMatrix) >= 150] 
+  if (sum(dim(ulMatrix)) == i) break                  
+}
+
+usersTable <- usersTable[match(rownames(ulMatrix), usersTable$userid), ]
+
+library(irlba)
+
+# Get 5 SVD dimensions for the user, like matrix
+svdUL <- irlba(ulMatrix, nv = 5)
+
+# Get SVD scores
+u <- svdUL$u
+v <- svdUL$v
+
+# Get the rotated V matrix:
+rotVmatrix <- unclass(varimax(svdUL$v)$loadings)
+
+# The cross-product of user like Matrix and rotated V Matrix
+rotUmatrix <- as.matrix(ulMatrix %*% rotVmatrix)
+
+# Correlate user traits and their SVD scores
+cor(rotUmatrix, usersTable[,-1], use = "pairwise")
+
+#libraries to be loaded
+library(corrplot)
+library(ggplot2)
+library(reshape2)
+
+# SVD
+top <- list()
+bottom <-list()
+for (i in 1:5) {
+  f <- order(rotVmatrix[ ,i])
+  temp <- tail(f, n = 10)
+  top[[i]]<-colnames(ulMatrix)[temp]  
+  temp <- head(f, n = 10)
+  bottom[[i]]<-colnames(ulMatrix)[temp]  
+}
